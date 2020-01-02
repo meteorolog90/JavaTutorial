4732ad3e4183493e76adb91d9063adef83ae6609
meteorolog90 <meteorolog90@hotmail.com>
2020-01-02 18:42:08 +0100

    add enum

---
+++ src/enumeracia/Enumeracia.java
@@ -0,0 +1,12 @@
+package enumeracia;
+
+public class Enumeracia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
